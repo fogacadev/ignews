726e999653707deccd1d38eafc0b340b5d951799
Felipe Fogaça <felipe.foogaca@gmail.com>
2022-01-22 21:07:43 -0300

    finished

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,8 @@ import { useSession, signIn } from 'next-auth/react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
+
 
 interface SubscribeButtonProps{
     priceId: string;
@@ -9,11 +11,19 @@ interface SubscribeButtonProps{
 
 export function SubscribeButton({ priceId } : SubscribeButtonProps){
     const {data: session} = useSession();
+    const router = useRouter();
 
+
+    
 
     async function handleSubscribe(){
         if(!session){
             signIn('github');
+            return;
+        }
+
+        if(session.activeSubscription){
+            router.push('/posts');
             return;
         }
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,6 +20,44 @@ export default NextAuth({
         
     ],
     callbacks:{
+      async session({session, token, user}){
+        try{
+
+          const userActiveSubscription = await fauna.query(
+            q.Get(
+              q.Intersection([
+                q.Match(
+                  q.Index('subscription_by_user_ref'),
+                  q.Select(
+                    ["ref", "id"],
+                    q.Get(
+                      q.Match(
+                        q.Index('user_by_email'),
+                        q.Casefold(session.user.email)
+                      )
+                    )
+                  )
+                ),
+                q.Match(
+                  q.Index('subscription_by_status'),
+                  'active'
+                )
+              ])
+            )
+          )
+          return {
+            ...session,
+            activeSubscription: userActiveSubscription
+          };
+        } catch (err) {
+          console.log('erro no fauna', err.message)
+
+          return {
+            ...session,
+            activeSubscription: null
+          };
+        }
+      },
             async signIn({user, account, profile}){
               const { email } = user;
 
